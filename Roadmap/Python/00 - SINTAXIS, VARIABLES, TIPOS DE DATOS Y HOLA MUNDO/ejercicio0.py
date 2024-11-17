dcad0cfea83b45269efdf465294117d74f157419
naodata-CS <naodata2002@gmail.com>
2024-11-17 18:48:43 -0500

    Ejercicio 0.

---
+++ Roadmap/Python/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/ejercicio0.py
@@ -0,0 +1,22 @@
+#This is the official webpage from python https://www.python.org/
+#Python don't support multi-line comments but ignore strings without a variable assigned
+"""
+    Like this, Python ignore
+"""
+import constantes
+
+a = "Hello world"
+
+print(a + " " + str(constantes.PINUMBER))
+
+#Constant variable puts in another file named modules and write in uppercase.
+#Import .py file with name of the file and when you put one variable or function write first filename.variable
+
+a = 12 #integer
+b = True #boolean
+c = 12.23 #float
+d = [2,3,4] #list
+e = "Python"
+print(type(a))
+
+print(f"Hello, {e}")
